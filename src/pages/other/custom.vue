fa043edd3135859b8b77f379b9ff777ac43b850f
虞力 <herman.yu@foxmail.com>
2021-06-09 16:13:04 +0800

    init

---
+++ src/pages/other/custom.vue
@@ -0,0 +1,32 @@
+<template>
+  <view class="custom">
+    test
+  </view>
+</template>
+
+<script>
+// 按需引入, 更小的应用体积
+import { AtButton, AtToast, AtNoticebar } from 'taro-ui-vue'
+import "taro-ui-vue/dist/style/components/button.scss"
+import "taro-ui-vue/dist/style/components/toast.scss"
+import "taro-ui-vue/dist/style/components/noticebar.scss"
+import './custom.scss'
+export default {
+    components: {
+  },
+  data () {
+    return {
+      msg: 'Hello world!',
+      show: false
+    }
+  },
+  methods: {
+    handleClick () {
+      this.show = true
+    },
+    handleClose () {
+      this.show = false
+    }
+  },
+    }
+</script>

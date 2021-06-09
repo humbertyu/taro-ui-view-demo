6b6a2c1854bc16a5594da98c7791fe64c6e418ca
虞力 <herman.yu@foxmail.com>
2021-06-09 18:43:35 +0800

    增加静态资源及重写开始页面

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,5 +1,6 @@
 export default {
   pages: [
+    'pages/basic/index',
     'pages/index/index',
     'pages/other/custom'
   ],
---
+++ src/pages/basic/index.vue
@@ -0,0 +1,112 @@
+<template>
+  <view class="page page-index">
+    <view class="logo">
+      <img src="exp.bdstatic.com/static/exp-pc/common-jquery/widget/search-box/img/logo_422270c.png" class='img' />
+    </view>
+    <view class='page-title'>Taro UI</view>
+    <view class='module-list'>
+      <view v-for="item in items"
+        class='module-list__item'
+        data-id={item.id}
+        data-name={item.title}
+        data-list={item.subpages}
+        onClick={this.gotoPanel}
+      >
+        <view class='module-list__icon'>
+          <img v-bind:src='item.icon' class='img' mode='widthFix' />
+        </view>
+        <view class='module-list__info'>
+          <view class='title'>{{item.title}}</view>
+          <view class='content'>{{item.content}}</view>
+        </view>
+        <view class='module-list__arrow'>
+          <view class='at-icon at-icon-chevron-right' />
+        </view>
+      </view>
+    </view>
+
+  </view>
+</template>
+
+<script>
+// 按需引入, 更小的应用体积
+import { AtIcon, AtButton, AtToast, AtNoticebar } from 'taro-ui-vue'
+import Taro from '@tarojs/taro'
+import "taro-ui-vue/dist/style/components/icon.scss";
+import "taro-ui-vue/dist/style/components/button.scss"
+import "taro-ui-vue/dist/style/components/toast.scss"
+import "taro-ui-vue/dist/style/components/noticebar.scss"
+import './index.scss'
+export default {
+    components: {
+      AtIcon,
+      AtButton,
+      AtToast,
+      AtNoticebar
+  },
+  data () {
+    return {
+      items: [
+        {
+          id: 'Basic',
+          title: '基础',
+          content: '包含颜色、文本、图标等',
+          icon: '../../assets/images/icon-list-basic.png'
+        },
+        {
+          id: 'View',
+          title: '视图',
+          content: '包含通告栏、标签、徽标等',
+          icon: '../../assets/images/icon-list-view.png'
+        },
+        {
+          id: 'Action',
+          title: '操作反馈',
+          content: '包含对话框、进度条、动作面板等',
+          icon: '../../assets/images/icon-list-view.png'
+        },
+        {
+          id: 'Form',
+          title: '表单',
+          content: '包含输入框、单选框、复选框等',
+          icon: '../../assets/images/icon-list-view.png'
+        },
+        {
+          id: 'Layout',
+          title: '布局',
+          content: '包含列表、浮层、卡片等',
+          icon: '../../assets/images/icon-list-view.png'
+        },
+        {
+          id: 'Navigation',
+          title: '导航',
+          content: '包含标签栏、导航栏、分段器等',
+          icon: '../../assets/images/icon-list-view.png'
+        },
+        {
+          id: 'Advanced',
+          title: '高阶组件',
+          content: '包含日历等',
+          icon: '../../assets/images/icon-list-view.png'
+        }
+      ],
+      msg: 'Hello world!',
+      show: false
+    }
+  },
+  methods: {
+    handleClick () {
+      this.show = true
+    },
+    handleClose () {
+      this.show = false
+    },
+    toCustom() {
+      this.show = true
+      Taro.redirectTo({
+        url: '/pages/other/custom'
+      })
+    }
+  }
+    }
+</script>
